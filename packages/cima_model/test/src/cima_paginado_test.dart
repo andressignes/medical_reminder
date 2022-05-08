07b89a9606f1da7a4f5efc1f212d48c714a27576
Andrés Signes <asignes86@gmail.com>
2022-05-08 12:04:25 +0200

    action params

--- packages/cima_model/test/src/cima_paginado_test.dart
+++ packages/cima_model/test/src/cima_paginado_test.dart
@@ -1,54 +1,31 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:cima_model/cima_model.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('CIMA Paginado', () {
-    // late final CimaPaginado cimaPaginadoObj;
-    // late final Map<String, dynamic> cimaPaginadoJson;
-    // setUpAll(() async {
-    //   cimaPaginadoJson = jsonDecode(
-    //     File('test/resources/cima_paginado.json').readAsStringSync(),
-    //   ) as Map<String, dynamic>;
-    //   cimaPaginadoObj = CimaPaginado(
-    //     pagina: cimaPaginadoJson['pagina'] as int,
-    //     resultados: cimaPaginadoJson['resultados'],
-    //     // cimaPaginadoJson['resultados'] as List<Map<String, dynamic>>?,
-    //     tamanioPagina: cimaPaginadoJson['tamanioPagina'] as int,
-    //     totalFilas: cimaPaginadoJson['totalFilas'] as int,
-    //   );
-    // });
+  group(
+    'CIMA Paginado',
+    () {
+      test('can be instantiated', () {
+        expect(const CimaPaginado(), isNotNull);
+      });
 
-    test('can be instantiated', () {
-      expect(const CimaPaginado(), isNotNull);
-    });
-
-    test('supports value comparisons', () {
-      expect(
-        CimaPaginado(
-              pagina: 1,
-              resultados: List<Map<String, dynamic>>.empty(growable: true),
-              tamanioPagina: 25,
-              totalFilas: 100,
-            ) ==
-            CimaPaginado(
-              pagina: 1,
-              resultados: List<Map<String, dynamic>>.empty(growable: true),
-              tamanioPagina: 25,
-              totalFilas: 100,
-            ),
-        isTrue,
-      );
-    });
-
-    // test('json serialization fromJson', () {
-    //   expect(CimaPaginado.fromJson(cimaPaginadoJson), cimaPaginadoObj);
-    // });
-    //
-    // test('json serialization toJson', () {
-    //   expect(cimaPaginadoObj.toJson(), cimaPaginadoJson);
-    // });
-  });
+      test('supports value comparisons', () {
+        expect(
+          CimaPaginado(
+                pagina: 1,
+                resultados: List<Map<String, dynamic>>.empty(growable: true),
+                tamanioPagina: 25,
+                totalFilas: 100,
+              ) ==
+              CimaPaginado(
+                pagina: 1,
+                resultados: List<Map<String, dynamic>>.empty(growable: true),
+                tamanioPagina: 25,
+                totalFilas: 100,
+              ),
+          isTrue,
+        );
+      });
+    },
+  );
 }
