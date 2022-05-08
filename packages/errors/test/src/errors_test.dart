360696210cd1842793c4eb722507c9f80799a337
Andrés Signes <asignes86@gmail.com>
2022-05-08 12:11:54 +0200

    errors tests

---
+++ packages/errors/test/src/errors_test.dart
@@ -0,0 +1,16 @@
+import 'package:errors/errors.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    late CacheFailure failure;
+
+    setUp(() {
+      failure = CacheFailure();
+    });
+
+    test('First Test', () {
+      expect(failure, isA<CacheFailure>());
+    });
+  });
+}
