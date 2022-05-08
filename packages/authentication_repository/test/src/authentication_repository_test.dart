148375e31928698914bbfd25af023577fce22ecd
Andrés Signes <asignes86@gmail.com>
2022-05-08 12:24:02 +0200

    authRepo tests

--- packages/authentication_repository/test/src/authentication_repository_test.dart
+++ packages/authentication_repository/test/src/authentication_repository_test.dart
@@ -1,11 +1,24 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:authentication_repository/authentication_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
+class MockFirebaseAuth extends Mock implements FirebaseAuth {}
+class MockUserDataSource extends Mock implements UserDataSource {}
+
 void main() {
+  final _auth = MockFirebaseAuth();
+  final _userDataSource = MockUserDataSource();
   group('AuthenticationRepository', () {
     test('can be instantiated', () {
-      expect(AuthenticationRepository(), isNotNull);
+      expect(
+        AuthenticationRepository(
+          firebaseAuth: _auth,
+          userDataSource: _userDataSource,
+        ),
+        isNotNull,
+      );
     });
   });
 }
