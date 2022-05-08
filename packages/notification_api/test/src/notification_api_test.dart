7d118022a17e15695e6ae3ae98d2707f06684f9e
Andrés Signes <asignes86@gmail.com>
2022-05-08 16:51:17 +0200

    testing core app

---
+++ packages/notification_api/test/src/notification_api_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:mocktail/mocktail.dart';
+import 'package:notification_api/notification_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('NotificationApi', () {
+    test('can be instantiated', () {
+      expect(NotificationApi(), isNotNull);
+    });
+  });
+}
--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -6,27 +6,38 @@
 // https://opensource.org/licenses/MIT.
 
 import 'package:authentication_repository/authentication_repository.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:medical_reminder/app/app.dart';
 import 'package:mocktail/mocktail.dart';
 
-class FirebaseAuthMock extends Mock implements FirebaseAuth {}
+class FirebaseAuthMock extends Mock implements firebase.FirebaseAuth {}
 
 class UserDataSourceMock extends Mock implements UserDataSource {}
 
+class AuthenticationRepositoryMock extends Mock
+    implements AuthenticationRepository {
+  @override
+  User get currentUser => User.empty;
+
+  @override
+  Stream<User> get user => Stream.value(User.empty);
+
+  @override
+  Future<User> get userAttributes => Future.value(User.empty);
+
+  @override
+  Stream<User> get userAttributesStream => Stream.value(User.empty);
+}
+
 void main() {
+  final _authRepository = AuthenticationRepositoryMock(
+
+  );
   group('App', () {
-    final firebaseAuth = FirebaseAuthMock();
-    final userDataSource = UserDataSourceMock();
     testWidgets('renders Home Page', (tester) async {
       await tester.pumpWidget(
-        App(
-          authenticationRepository: AuthenticationRepository(
-            firebaseAuth: firebaseAuth,
-            userDataSource: userDataSource,
-          ),
-        ),
+        App(authenticationRepository: _authRepository),
       );
       // expect(find.byType(CounterPage), findsOneWidget);
     });
